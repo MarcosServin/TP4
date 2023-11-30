697ec0d2e3a074d1832785ebce6cdda56ba14bbe
MarcosServin <marcosservin7711@gmail.com>
2023-11-30 13:47:54 -0300

    Color negro y div por 0

--- calculadora_funciones.py
+++ calculadora_funciones.py
@@ -8,12 +8,12 @@ class Calculadora:
         self.ventana.columnconfigure(0, weight=1)
         self.ventana.rowconfigure(0, weight=1)
 
-        self.frame_entrada=tk.Frame(self.ventana)
+        self.frame_entrada=tk.Frame(self.ventana,bg="#ffffff")
         self.frame_entrada.grid(row=0,column=0,sticky="nsew")
         self.frame_entrada.columnconfigure(0, weight=1)
         self.frame_entrada.rowconfigure(0, weight=1)
 
-        self.entrada=tk.Entry(self.frame_entrada,justify='right')
+        self.entrada=tk.Entry(self.frame_entrada,justify='right',font=("arial",50),fg="white",bg="black")
         self.entrada.grid(row=0,column=0,sticky="nsew")
 
         self.frame_calc = tk.Frame(self.ventana)
@@ -26,6 +26,8 @@ class Calculadora:
         self.frame_calc.rowconfigure(2, weight=1)
         self.frame_calc.columnconfigure(3, weight=1)
         self.frame_calc.rowconfigure(3, weight=1)
+        self.frame_calc.columnconfigure(4, weight=1)
+        self.frame_calc.rowconfigure(4, weight=1)
 
         self.crear_botones()
     
@@ -33,14 +35,12 @@ class Calculadora:
         calculo=self.entrada.get()
         indi=len(calculo)
         try:
+            self.entrada.delete(0,indi)
             calculo=eval(calculo)
-            self.entrada.delete(0,indi+1)
             self.entrada.insert(0,calculo)
         except:
-            self.entrada.delete(0,indi+1)
+            self.entrada.delete(0,indi)
             self.entrada.insert(0,"ERROR")
-            self.entrada.insert(0,"ERROR")
-            self.entrada.delete(0,indi+1)
     
     def insertar_valor(self,valor):
         texto=self.entrada.get()
@@ -49,30 +49,36 @@ class Calculadora:
             self.computo_final()
         elif valor=='←':
             self.entrada.delete(index-1)
+        elif valor=="CE":
+            self.entrada.delete(0,index)
         else:
             self.entrada.insert(index,valor)
 
     def crear_boton(self, valor, fila, columna):
-        button = tk.Button(self.frame_calc, text=str(valor),command=lambda:self.insertar_valor(valor))
-        button.grid(row=fila, column=columna,sticky='NSEW',padx=2,pady=2)
+        button = tk.Button(self.frame_calc, text=str(valor),command=lambda:self.insertar_valor(valor),fg="white",bg="black")
+        button.grid(row=fila, column=columna,sticky='NSEW')
 
     def crear_botones(self):
         self.crear_boton("*", 0, 0)
         self.crear_boton("/", 0, 1)
         self.crear_boton("=", 0, 2)
         self.crear_boton("←", 0, 3)
+        self.crear_boton("CE", 0, 4)
         self.crear_boton("7", 1, 0)
         self.crear_boton("8", 1, 1)
         self.crear_boton("9", 1, 2)
         self.crear_boton("+", 1, 3)
+        self.crear_boton("(", 1, 4)
         self.crear_boton("4", 2, 0)
         self.crear_boton("5", 2, 1)
         self.crear_boton("6", 2, 2)
         self.crear_boton("-", 2, 3)
+        self.crear_boton(")", 2, 4)
         self.crear_boton("1", 3, 0)
         self.crear_boton("2", 3, 1)
         self.crear_boton("3", 3, 2)
         self.crear_boton("0", 3, 3)
+        self.crear_boton(".", 3, 4)
 
 if __name__ == "__main__":
     ventana = tk.Tk()
